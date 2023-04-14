86a8cf7661da97dac71d88f94214b00fe5571574
Simon Nonnis <simon.nonnis@protonmail.com>
2023-04-14 21:02:24 +0300

    Add styles for NewNote

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,19 +1,42 @@
 export default function NewNote() {
   return (
-    <form method="post">
+    <form
+      method="post"
+      className="max-w-lg mx-auto my-12 p-8 rounded-md bg-lime-300 shadow-md text-center"
+    >
       <p>
-        <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required />
+        <label htmlFor="title" className="block mb-2 font-bold text-lime-700">
+          Title
+        </label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          className="block w-full px-1 py-2  bg-lime-400 rounded-sm  text-lime-700"
+          required
+        />
       </p>
 
       <p>
-        <label htmlFor="content">Title</label>
-        <textarea id="content" type="text" name="content" rows="5" required />
+        <label
+          htmlFor="content"
+          className="block mt-8 mb-2 font-bold text-lime-700"
+        >
+          Content
+        </label>
+        <textarea
+          id="content"
+          type="text"
+          name="content"
+          rows="5"
+          className="block w-full px-1 py-2  bg-lime-400 rounded-sm  text-lime-700"
+          required
+        />
       </p>
 
-      <div>
-        <button>Add Note</button>
-      </div>
+      <button className="inline-block cursor-pointer	py-3 px-8 mt-8 rounded-3xl	 bg-lime-400 hover:bg-lime-500 font-bold  text-lime-700 disabled:bg-lime-400 disabled:cursor-not-allowed	disabled:text-lime-500">
+        Add Note
+      </button>
     </form>
   );
 }
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,13 +1,9 @@
-import Title from "~/components/Title";
 import NewNote from "~/components/NewNote";
-import Link1 from "~/components/Link1";
 
 export default function NotesPage() {
   return (
     <main>
-      <Title>Notes</Title>
       <NewNote />
-      <Link1 to="/">Home Page</Link1>
     </main>
   );
 }
