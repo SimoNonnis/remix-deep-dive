a33abecf66cdb28d725f7fcdbebbf423197ed947
Simon Nonnis <simon.nonnis@protonmail.com>
2023-04-15 15:36:02 +0300

    Load data

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,15 +1,24 @@
-import { json, redirect } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 import { getStoredNotes, storeNotes } from "~/data/notes";
 
 import Title from "~/components/Title";
 import NewNote from "~/components/NewNote";
+import { links } from "~/root";
 
 export default function NotesPage() {
+  const notesList = useLoaderData();
+
   return (
     <main className="text-center mx-2">
       <Title>My Notes</Title>
       <NewNote />
+      <ul>
+        {notesList?.map((note) => (
+          <li>{note.title}</li>
+        ))}
+      </ul>
     </main>
   );
 }
@@ -17,6 +26,11 @@ export default function NotesPage() {
 export const meta = () => {
   return [{ title: "Remix Deep Dive | Notes" }];
 };
+
+export async function loader() {
+  const notesList = await getStoredNotes();
+  return notesList;
+}
 
 export async function action({ request }) {
   const formData = await request.formData();
